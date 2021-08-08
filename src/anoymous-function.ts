b393b6087f7bbf7783c823885e60b64453e247e1
teraman7 <puman0815@gmail.com>
2021-08-08 18:12:13 +0900

    lesson 2021-08-08

---
+++ src/anoymous-function.ts
@@ -0,0 +1,11 @@
+export {};
+
+let bmi: (height: number, weight: number) => number = function(
+    height: number,
+    weight: number
+): number {
+    return weight / (height * height);
+};
+
+console.log(bmi(1.78, 86));
+
